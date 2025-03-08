349eb00275ba447e0213af9df7b64fb7872f264b
elk <194042783+3elk@users.noreply.github.com>
2025-03-08 17:15:03 +0000

    (part of the click to enter page)

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-
+document.getElementById('enterButton').addEventListener('click', function() {
+    document.getElementById('overlay').classList.add('hidden');
+    setTimeout(() => {
+        document.getElementById('overlay').style.display = 'none';
+    }, 500);
+});
